ab5e327e6dc0708c6631424adfb8d8acee18b833
Matthew <69815486+matthew1611@users.noreply.github.com>
2024-10-07 16:02:10 -0500

    set day height & include text

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const calendar = document.querySelector('#app-calendar');
 
-console.log(calendar);
+const isWeekend = (day) => {
+  return day % 7 === 5 || day % 7 === 6;
+};
+
 for (let day = 1; day <= 31; day++) {
-  calendar.insertAdjacentHTML('beforeend', `<div class="day">${day}</div>`);
+  const date = new Date(2024, 9, day);
+  const dayName = new Intl.DateTimeFormat('en-US', { weekday: 'short' }).format(
+    date
+  );
+
+  const weekend = isWeekend(day);
+  calendar.insertAdjacentHTML(
+    'beforeend',
+    `<div class="day ${
+      weekend ? 'weekend' : ''
+    }"><div class="name">${dayName}</div>${day}</div>`
+  );
 }
