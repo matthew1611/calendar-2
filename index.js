368a1b3d62a72f03bae71faeac7a5f717beced4a
Matthew <69815486+matthew1611@users.noreply.github.com>
2024-10-09 11:45:01 -0500

    refactor toggle

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const isWeekend = (day) => {
   return day % 7 === 5 || day % 7 === 6;
 };
 
+let days = {
+  selected: 0,
+};
+
 for (let day = 1; day <= 31; day++) {
   const date = new Date(2024, 9, day);
   const dayName = new Intl.DateTimeFormat('en-US', { weekday: 'short' }).format(
@@ -13,19 +17,37 @@ for (let day = 1; day <= 31; day++) {
   const weekend = isWeekend(day);
 
   let name = '';
-
   if (day <= 7) {
     name = `<div class="name">${dayName}</div>`;
   }
+
   calendar.insertAdjacentHTML(
     'beforeend',
     `<div class="day ${weekend ? 'weekend' : ''}">${name}${day}</div>`
   );
 }
 
-document.querySelectorAll('#app-calendar .day').forEach((day) => {
-  day.addEventListener('click', (event) => {
-    event.currentTarget.classList.toggle('selected');
-    console.log(day.textContent);
-  });
+let daysSelected = [];
+
+const dayBoxes = document.querySelectorAll('#app-calendar .day');
+dayBoxes.forEach((day) => {
+  day.addEventListener('click', toggle);
+  // console.log(day.srcElement);
+  // saveDays();
 });
+
+function toggle(e) {
+  e.currentTarget.classList.toggle('selected');
+  console.log(e.target.attributes.class.value);
+  if (e.target.attributes.class.value === 'day selected') {
+    daysSelected.push(e.target.innerText);
+    console.log('selected');
+    console.log(daysSelected);
+  } else {
+    console.log('not selected');
+  }
+}
+
+// function saveDays() {
+//   localStorage.setItem('days');
+// }
